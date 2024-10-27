5b9c839e31d1d871bec9748c718c32b76d9ed6cb
Husnain Javed <tohusnainjaved@gmail.com>
2024-10-27 15:24:08 +0000

    conversion operations done

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -14,9 +14,59 @@ let valueInNumber = Number(score)
 // "true" => 1
 // "false" => 0
 
-let isLoggedIn = "hitesh"
+let isLoggedIn = "husnain"
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+//console.log(booleanIsLoggedIn);
 
 // "0" => False; "1"=> True
 // "husnain" => True
+
+
+// ***********************************************OPERATIONS********************************************//
+
+let value = 3
+let neg = -value
+// console.log(neg);
+
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/2);
+// console.log(2%3);
+
+let str1 = "Hello"
+let str2 = " Husnain"
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("2" + "2");      => 22
+// console.log("2" + 1);        => 21
+// console.log(2 + 1);          => 3
+// console.log(2 + "1" + "2");  => 212
+// console.log(2 + 1 + "2");    => 32
+
+// console.log(true);           =>true
+// console.log(+true);          =>1   
+// console.log(false);          =>false
+// console.log(+false);         =>0
+// console.log(+12);            =>12
+// console.log(+"");            =>0
+
+ let gamecounterx = 100
+ let gamecountery = ++gamecounterx
+// console.log(gamecounterx, gamecountery);
+    //  Expected Result = 101,  101
+
+
+let gamecounter1 = 125
+let gamecounter2 = gamecountera++
+// console.log(gamecountera, gamecounterb);
+    //  Expected Result = 126,   125
+
+
+
+
+
+
