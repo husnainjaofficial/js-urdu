be677dc485f6ebff93b6c273237c66776ec6d5fd
Husnain Javed <tohusnainjaved@gmail.com>
2024-10-27 06:16:05 +0000

    Conversiom of Datatypes

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,22 @@
+let score = "33"
+
+//console.log(typeof score)
+//console.log(typeof (score))
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber)
+//console.log(valueInNumber)
+
+
+// "33"=> 33
+// "33abs" => NaN
+// "husnain" => NaN
+// "true" => 1
+// "false" => 0
+
+let isLoggedIn = "hitesh"
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+// "0" => False; "1"=> True
+// "husnain" => True
