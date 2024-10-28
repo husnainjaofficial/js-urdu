1a210fbedaa75aa7615a0cd72050e6869beda9bf
Husnain Javed <tohusnainjaved@gmail.com>
2024-10-28 04:01:04 +0000

    Datatypes Comparison

---
+++ 01_basics/04_comparisons.js
@@ -0,0 +1,19 @@
+// console.log(2 > 1);
+// console.log(2 < 1);
+// console.log(2 >= 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+// console.log("2" > "1");    => True
+// console.log("2" > 3);      => False
+
+// console.log(null > 0);     => False
+// console.log(null == 0);      => false     Bcz comparisons < > work diiferently than Equality check == that is "null is converted to 0 in comparison" 
+// console.log(null >= 0);     => True
+
+
+//   ===   ( Strict Check )  also check Datatype
+
+
+console.log("2" === 2);
+
